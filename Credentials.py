d08985fd81aa1b1effdffbf06964331e60d2971e
j-dover <aldover27@gmail.com>
2022-07-05 09:49:38 -0400

    update Credentials class with getters

--- Credentials.py
+++ Credentials.py
@@ -3,8 +3,26 @@ import json
 class Credentials:
     
   def __init__(self, response_parameters):
-    self.credential_params = {}
+    self.__store = {}
 
     for parameter in response_parameters:
       name = parameter['Name']
-      self.credential_params[name] = parameter['Value']
+      self.__store[name] = parameter['Value']
+  
+  def get_store(self):
+    return self.__store
+  
+  def get_recipient_email(self):
+    return self.__store['email_recipient']
+
+  def get_sender_email(self):
+    return self.__store['email_sender']
+  
+  def get_user_agent(self):
+    return self.__store['user_agent']
+  
+  def get_client_id(self):
+    return self.__store['client_id']
+
+  def get_client_secret(self):
+    return self.__store['client_secret']
